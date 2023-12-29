1b67a349a5d8eafb63f2dd424ee09e45acf56c7b
classyoon <96508736+classyoon@users.noreply.github.com>
2023-12-28 18:12:17 -0800

    Initial Commit

---
+++ TryingDataService/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TryingDataService
+//
+//  Created by Conner Yoon on 12/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TryingDataService/TryingDataServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryingDataServiceApp.swift
+//  TryingDataService
+//
+//  Created by Conner Yoon on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TryingDataServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
